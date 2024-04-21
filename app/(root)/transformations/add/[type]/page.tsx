6e818607f6254bab61b2c99ba8d8859f27b61ef1
ispasdani <ispasdani14@gmail.com>
2024-04-22 00:15:23 +0300

    dsad

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,20 @@
+import Header from "@/components/shared/Header";
 import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { auth } from "@clerk/nextjs";
 
-const AddTransformationsTypePage = () => {
-  return <div>AddTransformationsTypePage</div>;
+const AddTransformationsTypePage = ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <TransformationForm action="Add" userId={} />
+    </>
+  );
 };
 
 export default AddTransformationsTypePage;
