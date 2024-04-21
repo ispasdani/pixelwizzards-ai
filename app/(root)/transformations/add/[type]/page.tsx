236a792fd4f263ea6ce45f1b92b60879cd8a6715
ispasdani <ispasdani14@gmail.com>
2024-04-22 00:32:50 +0300

    gsg

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -3,16 +3,29 @@ import React from "react";
 import { transformationTypes } from "@/constants";
 import TransformationForm from "@/components/shared/TransformationForm";
 import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-const AddTransformationsTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationsTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
   const { userId } = auth();
   const transformation = transformationTypes[type];
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
 
   return (
     <>
       <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-      <TransformationForm action="Add" userId={} />
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
     </>
   );
 };
